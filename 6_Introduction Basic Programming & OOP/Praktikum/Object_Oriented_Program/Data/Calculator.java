2d76099dc5f9c5597d86698a83aa34c158ccb984
Moses <eliasemoses123@gmail.com>
2023-03-16 23:51:14 +0700

    Section 6_Introduction Basic Programming & OOP

---
+++ 6_Introduction Basic Programming & OOP/Praktikum/Object_Oriented_Program/Data/Calculator.java
@@ -0,0 +1,29 @@
+package Object_Oriented_Program.Data;
+
+public class Calculator {
+
+    // Method Penjumlahan
+    public Integer add(Integer x, Integer y) {
+        Integer hasil = x + y;
+        return hasil;
+    }
+
+    // Method Pengurangan
+    public Integer substract(Integer x, Integer y) {
+        Integer hasil = x - y;
+        return hasil;
+    }
+
+    // Method Perkalian
+    public Integer multiply(Integer x, Integer y) {
+        Integer hasil = x * y;
+        return hasil;
+    }
+
+    // Method Pembagian
+    public Integer divide(Integer x, Integer y) {
+        Integer hasil = x / y;
+        return hasil;
+    }
+}
+
--- 6_Introduction Basic Programming & OOP/Praktikum/Object_Oriented_Program/SP1_SP2.java
+++ 6_Introduction Basic Programming & OOP/Praktikum/Object_Oriented_Program/SP1_SP2.java
@@ -1,9 +1,11 @@
 package Object_Oriented_Program;
 
+import Object_Oriented_Program.Data.Calculator;
 import Object_Oriented_Program.Data.Product;
 
 public class SP1_SP2 {
     public static void main(String[] args) {
+
         System.out.println("===");
         System.out.println("INFO PRODUK");
         Product product = new Product();
@@ -42,6 +44,13 @@ public class SP1_SP2 {
         System.out.println("harga: " + product.getHarga());
         System.out.println("jumlah stok: " + product.getJumlahstok());
         System.out.println("===");
+
+        // Calculator
+        Calculator calc = new Calculator();
+        System.out.println("Penjumlahan: " + calc.add(3,4));
+        System.out.println("Pengurangan: " + calc.substract(15,4));
+        System.out.println("Perkalian: " + calc.multiply(10,10));
+        System.out.println("Pembagian: " + calc.divide(12,3));
 
     }
 }
